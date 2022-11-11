a97acf5bc1064d90917be15ec4c3dd495ea9258e
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-11-10 20:44:43 -0800

    set up server and router using express

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,12 @@
 // implement your posts router here
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'router server response'
+    });
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,16 @@
 // implement your server here
+const express = require('express');
+
 // require your posts router and connect it here
+const postsRouter = require('./posts/posts-router');
+
+const server = express();
+
+server.use('/api/posts', postsRouter);
+server.use('/', (req, res) => {
+    res.status(200).json({
+        message: 'standard server response'
+    });
+});
+
+module.exports = server;
