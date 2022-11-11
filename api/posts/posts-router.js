7939502876e543b2d454d92669e944801f2d9ee9
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-11-11 13:28:49 -0800

    created put response

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -52,17 +52,29 @@ router.post('/', async (req, res) => {
     }
 });
 
-// router.put('/:id', (req, res) => {
-//     try {
-//         const {id} = req.params;
-//         const {title, contents} = req.body;
-//         console.log(id, title, contents);
-//     } catch(err) {
-//         res.status(500).json({
-//             message: "The post information could not be modified"
-//         })
-//     }
-// });
+router.put('/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const {title, contents} = req.body;
+        if(!await Posts.findById(id)) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else if(!title || !contents) {
+            res.status(400).json({
+                message: "Please provide title and contents for the post"
+            })
+        } else {
+            await Posts.update(id, {title, contents});
+            const updatedPost = await Posts.findById(id);
+            res.status(200).json(updatedPost);
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: "The post information could not be modified"
+        })
+    }
+});
 
 // throw new Error();
 
