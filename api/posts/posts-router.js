9eb265a5b50d3f9f6264c854553800bdecb57957
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-11-11 14:48:40 -0800

    completed delete and get responses

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -76,6 +76,43 @@ router.put('/:id', async (req, res) => {
     }
 });
 
-// throw new Error();
+router.delete('/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const postToDelete = await Posts.findById(id);
+        if(!postToDelete) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            await Posts.remove(id);
+            res.status(200).json(postToDelete);
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: "The post could not be removed"
+        })
+    }
+})
+
+router.get('/:id/comments', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const found = await Posts.findById(id);
+        if(!found) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else {
+            const comments = await Posts.findPostComments(id);
+            console.log(comments);
+            res.status(200).json(comments);
+        }
+    } catch(err) {
+        res.status(500).json({
+            message: "The comments information could not be retrieved"
+        })
+    }
+})
 
 module.exports = router;
